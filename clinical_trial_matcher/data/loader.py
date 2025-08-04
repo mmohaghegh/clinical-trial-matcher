739e4231db2667bfb34c964dcbc96a4c9a704d31
Mohammadreza Nejad <mohammadreza.nejad@adition.com>
2025-08-05 00:13:29 +0200

    first working version of the trial matcher.

--- clinical_trial_matcher/data/loader.py
+++ clinical_trial_matcher/data/loader.py
@@ -1,4 +1,6 @@
 import json
+import pickle
+
 import torch
 import requests
 import numpy as np
@@ -7,12 +9,32 @@ from tqdm import tqdm
 from pathlib import Path
 from functools import reduce
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Optional, Union, Generator
+
 from clinical_trial_matcher.model.embeddings import BertEmbedding
 
 class DataLoader:
-    def __init__(self, data: list):
+    def __init__(
+            self,
+            data: list,
+            page_size: Optional[int] = None,
+            page_num: Optional[int] = None,
+            api_url: Optional[str] = None,
+            condition: Optional[str] = None,
+            next_page_token: Optional[str] = None
+    ):
         self.data = data
+        self.page_size = page_size
+        self.page_num = page_num
+        self.api_url = api_url
+        self.condition = condition
+        self.next_page_token = next_page_token
+        self.ids: list[str] = list()
+        self._extract_ids()
+
+    def _extract_ids(self):
+        for item in self.data:
+            self.ids.append(item['protocolSection']['identificationModule']['nctId'])
 
     @classmethod
     def from_api(
@@ -37,13 +59,74 @@ class DataLoader:
             r = requests.get(api_url, params=params)
             data = r.json()
             studies.extend(data["studies"])
-        return cls(data=studies)
+        return cls(
+            data=studies,
+            page_size=page_size,
+            page_num=num_pages,
+            api_url=api_url,
+            condition=condition,
+            next_page_token=data.get("nextPageToken", None)
+        )
 
     @classmethod
     def from_file(cls, file_path: Path):
         with open(file_path, "r") as f:
             data = json.load(f)
         return cls(data=data)
+
+    def get_ntcIds_from_indices(self, indices: list[int]) -> list[str]:
+        """
+        Get NCT IDs from the data based on provided indices.
+
+        Args:
+            indices (list[int]): List of indices to retrieve NCT IDs from.
+
+        Returns:
+            list[str]: List of NCT IDs corresponding to the provided indices.
+        """
+        return [self.ids[i] for i in indices if i < len(self.ids)]
+
+    def generate_entire_data_from_nctIds(self, nct_ids: list[str], fmt: str = 'json') -> Generator[dict, None, None]:
+        if self.api_url is None:
+            raise ValueError("DataLoader instance is not generated from API, hence cannot fetch data by NCT IDs.")
+        for nct_id in nct_ids:
+            params = {
+                "format": fmt,
+                "query.id": nct_id,
+            }
+            r = requests.get(self.api_url, params=params)
+            if r.status_code != 200:
+                raise ValueError(f"Failed to fetch data for NCT ID {nct_id}. Status code: {r.status_code}")
+            data = r.json()
+            yield data
+
+    def generate_entire_data_from_indices(self, indices: list[int], fmt: str = 'json') -> Generator[dict, None, None]:
+        """
+        Generate entire data from the indices of the DataLoader instance.
+
+        Args:
+            fmt (str): Format of the data to be fetched. Default is 'json'.
+
+        Yields:
+            dict: Data corresponding to each index in the DataLoader.
+        """
+        ntcIds = self.get_ntcIds_from_indices(indices)
+        if not ntcIds:
+            raise ValueError("No NCT IDs found for the provided indices.")
+        return self.generate_entire_data_from_nctIds(ntcIds, fmt=fmt)
+
+    def save_api_state(self, path: Path):
+        if self.api_url is None:
+            raise ValueError("DataLoader instance is not generated from API, hence cannot save API state.")
+        data_loader = DataLoader(
+            data=list(),
+            page_size=self.page_size,
+            page_num=self.page_num,
+            api_url=self.api_url,
+            condition=self.condition,
+            next_page_token=self.next_page_token
+        )
+        data_loader.save_to_file(path)
 
     def __iter__(self):
         for item in self.data:
@@ -59,7 +142,6 @@ class DataLoader:
         with open(file_path, "w") as f:
             json.dump(self.data, f)
 
-
 @dataclass
 class EmbeddingResult:
     data_description: str
@@ -67,28 +149,74 @@ class EmbeddingResult:
     similarity_scores: np.ndarray = None
     top_k_indices: np.ndarray = None
     top_k_scores: np.ndarray = None
+    embedding_model: BertEmbedding = None
+    model_name: str = None
 
-    def compute_similarity(self, text_embedding: torch.Tensor) -> None:
+    def tokenize_and_embed(self, text: str) -> torch.Tensor:
+        if self.embedding_model is None:
+            self.embedding_model = BertEmbedding(self.model_name)
+        return self.embedding_model.embed(text)
+
+    def compute_similarity(self, text: str) -> np.ndarray:
+        text_embedding = self.tokenize_and_embed(text)
         similarities = torch.nn.functional.cosine_similarity(
             text_embedding.unsqueeze(0),
             self.reference_embeddings,
             dim=1
         ).numpy()
-        self.similarity_scores = similarities.flatten()
+        return similarities.flatten()
 
-    def compute_top_k_similarities(self, text_embedding: torch.Tensor, k: int = 5) -> None:
-        similarities = self.compute_similarity(text_embedding)
+    def compute_top_k_similarities(self, text: str, k: int = 5) -> None:
+        similarities = self.compute_similarity(text)
         top_k_indices = np.argsort(similarities)[-k:][::-1]
         self.top_k_indices, self.top_k_scores = top_k_indices, similarities[top_k_indices]
 
+@dataclass
+class EmbeddingResults:
+    data_description: list[str]
+    embedding_results: list[EmbeddingResult]
+
+    @classmethod
+    def from_a_embedding_result(cls, embedding_result: EmbeddingResult):
+        return cls(
+            data_description=[embedding_result.data_description],
+            embedding_results=[embedding_result]
+        )
+
+    # Method to add a new embedding result
+    def add_embedding_result(self, data_description: str, reference_embeddings: torch.Tensor, model_name: str) -> None:
+        result = EmbeddingResult(
+            data_description=data_description,
+            reference_embeddings=reference_embeddings,
+            model_name=model_name
+        )
+        self.embedding_results.append(result)
+        self.data_description.append(data_description)
+
+    def get_similarity_scores(self, text: str) -> np.ndarray:
+        similarities = []
+        for result in self.embedding_results:
+            similarities.append(result.compute_similarity(text))
+        return np.mean(similarities, axis=0)
+
+    def get_top_k_indices_based_on_similarity(self, text: str, k: int = 5) -> np.ndarray:
+        avg_similarities = self.get_similarity_scores(text)
+        top_k_indices = np.argsort(avg_similarities)[-k:][::-1]
+        return top_k_indices
+
+
+
+    def write_to_file(self, file_path: Path):
+        with open(file_path, "wb") as f:
+            pickle.dump(self.embedding_results, f)
 
 class EmbeddedData:
-    def __init__(self, data_loader: DataLoader, embedding_model: Optional[BertEmbedding] = None):
+    def __init__(self, data_loader: DataLoader, model_name = None):
         self.data_loader = data_loader
-        if embedding_model is None:
+        if model_name is None:
             self.embedding_model = BertEmbedding()
         else:
-            self.embedding_model = BertEmbedding(embedding_model)
+            self.embedding_model = BertEmbedding(model_name)
 
     def embed_for_key(self, key: Union[str, list[str]]):
         if isinstance(key, str):
--- scripts/initial_data_loading.py
+++ scripts/initial_data_loading.py
@@ -4,41 +4,57 @@ from pathlib import Path
 
 from tqdm import tqdm
 
-from clinical_trial_matcher.data.loader import EmbeddedData, DataLoader
+from clinical_trial_matcher.data.loader import EmbeddedData, DataLoader, EmbeddingResult, EmbeddingResults
 
-READ_EXISTING_DATA = True
+READ_EXISTING_DATA = False
+STORE_DATA = True
+MODEL_NAME = 'dmis-lab/biobert-base-cased-v1.1'
 data_path = Path('./data')
 file_path = data_path / "clinical_trials.json"
 
 if READ_EXISTING_DATA:
-    with open(file_path, "r") as f:
-        studies = json.load(f)
-    print(f"Loaded {len(studies)} studies from existing data.")
-
+    data_obj = DataLoader.from_file(file_path)
+    print(f"Loaded {len(data_obj)} studies from existing data.")
 else:
     params = {
         "format": "json",
         "query.cond": "cancer",
-        "pageSize": 10000,
+        "pageSize": 10,
     }
     studies = []
     url = "https://clinicaltrials.gov/api/v2/studies"
-    r = requests.get(url, params=params)
-    data = r.json()
-    studies.extend(data["studies"])
-    for i in tqdm(range(10), desc="Loading studies page by page"):
-        params["pageToken"] = data["nextPageToken"]
-        r = requests.get(url, params=params)
-        data = r.json()
-        studies.extend(data["studies"])
+    data_obj = DataLoader.from_api(
+        api_url=url,
+        fmt=params["format"],
+        condition=params["query.cond"],
+        page_size=params["pageSize"],
+        num_pages=1
+    )
+    if STORE_DATA:
+        data_obj.save_to_file(file_path)
 
-    with open(file_path, "w") as f:
-        json.dump(studies, f)
-
-data_obj = DataLoader(studies)
-embedding_data = EmbeddedData(data_obj)
-embedding_data.embed_for_key(['protocolSection', 'identificationModule', 'briefTitle'])
-print(studies[0]['protocolSection']['identificationModule']['briefTitle'])
-print(studies[0]['protocolSection']['descriptionModule']['briefSummary'])
-print(studies[0]['protocolSection']['eligibilityModule']['eligibilityCriteria'])
-print(studies[0]['protocolSection']['conditionsModule']['conditions'])
+embedding_data = EmbeddedData(data_obj, model_name=MODEL_NAME)
+embedding_result = embedding_data.embed_for_key(['protocolSection', 'identificationModule', 'briefTitle'])
+embedding_results = EmbeddingResults.from_a_embedding_result(
+    EmbeddingResult(data_description='brief title of clinical trials', reference_embeddings=embedding_result, model_name=MODEL_NAME),
+)
+embedding_results.add_embedding_result(
+    data_description='brief summary of clinical trials',
+    reference_embeddings=embedding_data.embed_for_key(['protocolSection', 'descriptionModule', 'briefSummary']),
+    model_name=MODEL_NAME
+)
+embedding_results.add_embedding_result(
+    data_description='eligibility criteria of clinical trials',
+    reference_embeddings=embedding_data.embed_for_key(['protocolSection', 'eligibilityModule', 'eligibilityCriteria']),
+    model_name=MODEL_NAME
+)
+# embedding_results.add_embedding_result(
+#     data_description='conditions of clinical trials',
+#     reference_embeddings=embedding_data.embed_for_key(['protocolSection', 'conditionsModule', 'conditions'])
+# )
+query_text = "breast cancer treatment"
+top_5_indices = embedding_results.get_top_k_indices_based_on_similarity(query_text, k=5)
+top_ntcIds = data_obj.get_ntcIds_from_indices(top_5_indices)
+string_decorators = '#' * 50
+for data in data_obj.generate_entire_data_from_nctIds(top_ntcIds):
+    print(string_decorators, '\n', json.dumps(data, indent=4), '\n', string_decorators)
